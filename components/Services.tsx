e6c88d208aa11d8655e69cbd227dc01c7bac0950
Masood ul Rehman <masooddulrehman@gmail.com>
2023-09-14 01:13:45 +0500

    completed service page added scrool

--- components/Services.tsx
+++ components/Services.tsx
@@ -2,25 +2,15 @@ import Image from "next/image";
 
 const Services = () => {
   return (
-    <div className=" mt-[20vw]">
+    <div className=" mt-[20vw] mb-[20vw]">
       <div className="grid grid-services">
-        <div>
-          <Image
-            src={"/assets/images/1.png"}
-            width={700}
-            height={100}
-            alt="img1"
-          />
-          {/* <img src="/assets/images/1.png" alt="" /> */}
+        <div className="bg-[url('/assets/images/1.jpeg')] bg-cover bg-no-repeat rounded-[60px] h-[450px] relative">
+          <div className="bg-bgClr relative z-20 w-fit px-10 py-5 rounded-full top-[82%] left-6">
+            <h2 className="text-white font-medium">Home Insurance</h2>
+          </div>
         </div>
-        <div>
-          {/* <Image
-            src={"/assets/images/2.png"}
-            width={320}
-            height={0}
-            alt="img1"
-          /> */}
-          <img src="/assets/images/2.png" alt="" />
+        <div className="flex justify-center">
+          <div className="bg-[url('/assets/images/2.jpeg')] bg-right bg-cover bg-no-repeat rounded-full h-[450px] w-[75%] "></div>
         </div>
 
         <div className="bg-bgClr px-10 pt-8 pb-10 rounded-[80px] flex flex-col justify-between ">
@@ -39,25 +29,22 @@ const Services = () => {
           </button>
         </div>
       </div>
-      <div className="grid grid-cols-3">
-        <Image
-          src={"/assets/images/3.png"}
-          width={100}
-          height={100}
-          alt="img1"
-        />{" "}
-        <Image
-          src={"/assets/images/4.png"}
-          width={100}
-          height={100}
-          alt="img1"
-        />
-        <Image
-          src={"/assets/images/5.png"}
-          width={100}
-          height={100}
-          alt="img1"
-        />
+      <div className="grid grid-cols-3 gap-8 mt-14">
+        <div className="bg-[url('/assets/images/3.jpeg')] bg-cover bg-center  bg-no-repeat rounded-[50px]  h-[300px] relative">
+          <div className="bg-bgClr relative z-20 w-fit px-10 py-5 rounded-full top-[72%] left-6">
+            <h2 className="text-white font-medium">Vehicle damage</h2>
+          </div>
+        </div>
+        <div className="bg-[url('/assets/images/4.jpeg')] bg-cover  bg-no-repeat rounded-[50px]  h-[300px] relative">
+          <div className="bg-bgClr relative z-20 w-fit px-10 py-5 rounded-full top-[72%] left-6">
+            <h2 className="text-white font-medium">Vehicle damage</h2>
+          </div>
+        </div>
+        <div className="bg-[url('/assets/images/5.jpeg')] bg-cover bg-no-repeat rounded-[50px]  h-[300px] relative">
+          <div className="bg-bgClr relative z-20 w-fit px-10 py-5 rounded-full top-[72%] left-6">
+            <h2 className="text-white font-medium">Vehicle damage</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
